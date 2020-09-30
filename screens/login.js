f0e004a76bb5613f487393b6e2142e411cad1a40
GitHub-star-arch <63858220+GitHub-star-arch@users.noreply.github.com>
2020-09-30 16:31:10 +0530

    Add files via upload

---
+++ screens/login.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Text, View, StyleSheet, KeyboardAvoidingView, ToastAndroid, Image, TextInput} from 'react-native';
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import * as Permissions from 'expo-permissions';
+import db from '../config.js';
+import * as firebase from 'firebase';
+export default class Login extends React.Component {
+    <View style={stylez.Searchsty}>
+    <Text>Story Title</Text>
+    <TextInput value="Story Name"/>
+    <Text>Author</Text>
+    <TextInput value="Name"/>
+    <Text>Story</Text>
+    <TextInput value="Story"/>
+    <View/>
+}
+const stylez = StyleSheet.create({
+    Searchsty:{
+        justifyContent:'center',
+        backgroundColor:'green',
+        alignText:'center',
+        Textsize:'10',
+        fontWeight:'5',
+        Textcolor:'Red',
+
+    }
+})
